1146e140d650f179e834bdc4cb9d537b3b330331
Hanna Lee <mani21na@gmail.com>
2020-02-08 00:36:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,4 @@
 class StudentsController < ApplicationController
-  def index
-  end
-
   def show
     @student = Student.find(params[:id])
   end
@@ -10,25 +7,19 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
-  def create
-    @student = Student.create(student_params)
-    
-    redirect_to student_path(@student)
-  end
-
   def edit
     @student = Student.find(params[:id])
   end
 
-  def update
-    @student = Student.find(params[:id])
-    @student.update(student_params)
-    
+  def create
+    @student = Student.new(params.require(:student))
+    @student.save
     redirect_to student_path(@student)
   end
 
-  private
-  def student_params
-    params.require(:student).permit(:first_name, :last_name)
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
   end
 end
