51625d53c5452c5c1abf517d12965218980d408a
Hanna Lee <mani21na@gmail.com>
2020-02-08 00:38:28 +0000

    Automatically backed up by Learn

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,2 +1,11 @@
-class SchoolClass < ActiveRecord::Base
-end
+<h3>School Class Form</h3>
+
+<%= form_for(@school_class) do |f| %>
+  <label>Title:</label><br>
+  <%= f.text_field :title %><br>
+
+  <label>Room Number:</label><br>
+  <%= f.number_field :room_number %><br>
+
+  <%= f.submit %>
+<% end %>
